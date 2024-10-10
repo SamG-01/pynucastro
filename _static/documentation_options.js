9c2d010bc24d56caeca6c87df0243b4af6d68232
SamG-01 <SamG-01@users.noreply.github.com>
2024-10-10 09:46:03 +0000

    deploy: 73a248eb52be98d3e086fb45e2429ae1e653ca0e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.2.0.post63+g04c295e3',
+    VERSION: '2.2.0.post64+g73a248eb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
