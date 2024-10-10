efad435aa9bd50c1851f654a9ad51dbc7221f61e
SamG-01 <SamG-01@users.noreply.github.com>
2024-10-10 21:48:19 +0000

    deploy: 955a3a41cc6e46c29e52f21add7af26235192c05

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.2.0.post81+gbf7f1740',
+    VERSION: '2.2.0.post82+g955a3a41',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
